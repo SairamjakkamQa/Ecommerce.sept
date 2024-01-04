2be5e7c058a5f18d6dd5afb0a3df63cc35083689
sai ram <jakkamsairam3@gmail.com>
2024-01-04 12:19:29 +0530

    Register added

--- src/main/java/com/pom/LoginFunctionality.java
+++ src/main/java/com/pom/LoginFunctionality.java
@@ -1,9 +1,47 @@
 package com.pom;
 
-public class LoginFunctionality {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.base.TestBase;
+
+public class LoginFunctionality extends TestBase {
 
 	
 	//Webelements address and initiate this by using pagefactory class 
+	
+	@FindBy (id="si-email")
+	WebElement user;
+	
+	@FindBy (id="si-password")
+	WebElement password;
+	
+	@FindBy (xpath="//button[text()='Sign in']")
+	WebElement submit;
+	
+	
+	public LoginFunctionality(WebDriver driver) {
+		
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+
+
+	public Homepage doLogin() {
+		
+		user.sendKeys(prop.getProperty("username"));
+		password.sendKeys(prop.getProperty("password"));
+		submit.click();
+		
+		return new Homepage();
+		
+	}
+	
+	
+	
 	
 	
 }
--- src/test/java/com/testcases/Logintest.java
+++ src/test/java/com/testcases/Logintest.java
@@ -1,5 +1,41 @@
 package com.testcases;
 
-public class Logintest {
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.base.TestBase;
+import com.pom.LoginFunctionality;
+
+public class Logintest extends TestBase {
+
+	LoginFunctionality login;
+
+	@BeforeMethod
+
+	public void Setup() {
+
+		Initialization();
+		login=new LoginFunctionality(driver);
+
+	}
+
+	@Test
+	public void verifyLogin() {
+
+		login.doLogin();
+
+		String homepageurl = driver.getCurrentUrl();
+
+		Assert.assertEquals("https://e-quarz.com/", homepageurl);
+	}
+
+	@AfterMethod
+
+	public void tearDown() {
+
+		//driver.close();
+	}
 
 }
